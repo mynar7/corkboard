84da9d3c12553b146110dadf806e942034ae3eff
Jonathan Disla <jon.disla@gmail.com>
2018-06-07 20:10:29 -0400

    resize images

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3307;
 const db = require("./models/index.js");
 const path = require("path");
 const apiRouter = require("./routes/apiRoutes.js");
